6a850447c1d683e9d989de20a30c49b8d2d32957
Lucas Paiva <paival907@gmail.com>
2026-06-25 10:22:54 -0300

    att

---
+++ api/prisma/migrations/20260625132247_endereco_loja/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Loja" ADD COLUMN     "bairro" TEXT,
+ADD COLUMN     "cep" TEXT,
+ADD COLUMN     "cidade" TEXT,
+ADD COLUMN     "endereco" TEXT,
+ADD COLUMN     "estado" TEXT,
+ADD COLUMN     "numero" TEXT;
